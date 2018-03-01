85cfa4fffc0c2225d8b6d55db713b40b743cf280
Elshibani Omar <amr.omar80@gmail.com>
2018-02-28 20:42:32 -0700

    add some functionalities

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,16 +1,11 @@
-//Dependency
-var path = require('path');
+var path = require("path");
 //route
-module.exports = function(app){
+module.exports = function(app) {
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname + "/../public/survey.html"));
+  });
 
-//GET request 
-	app.get('/survey', function(req, res){
-		res.sendFile(path.join(__dirname + '/../public/survey.html'));
-	});
-
-	// If no matching route is found default to home
-	app.use(function(req, res){
-		res.sendFile(path.join(__dirname + '/../public/home.html'));
-	});
-
-}
+  app.use(function(req, res) {
+    res.sendFile(path.join(__dirname + "/../public/home.html"));
+  });
+};
