1f3691742aaebf0503292a3c56a52bf7905f511c
Elshibani Omar <amr.omar80@gmail.com>
2018-02-28 20:15:30 -0700

    Add all files

---
+++ app/data/friends.js
@@ -0,0 +1,63 @@
+var friendsArray = [
+  {
+    "name": "Sheldon",
+    "photo":
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BZjg4MGNlZDgtMmM5OC00ZmMxLTg3Y2EtZmZjOGJlNDU4NGNhXkEyXkFqcGdeQXVyMTgxOTIzNzk@._V1_UY666_CR259,0,666,666_AL_.jpg",
+    "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
+  {
+    "name": "Leonard",
+    "photo":
+      "http://listas.eleconomista.es/system/items/000/024/259/medium/leonard.jpg?1386116558",
+    "scores": [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
+  },
+  {
+    "name": "Raj",
+    "photo":
+      "http://comedycentral-hu.mtvnimages.com/images/big-bang-theory-akkoresmost-raj-2.jpg?width=600",
+    "scores": [5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
+  },
+  {
+    "name": "Howard",
+    "photo":
+      "https://pbs.twimg.com/profile_images/618761009696604160/yErRfXwS.jpg",
+    "scores": [3, 3, 4, 2, 2, 1, 3, 2, 2, 3]
+  },
+  {
+    "name": "Penny",
+    "photo": "http://s3.r29static.com//bin/entry/568/x,80/1638541/image.jpg",
+    "scores": [4, 3, 4, 1, 5, 2, 5, 3, 1, 4]
+  },
+  {
+    "name": "Bernadette",
+    "photo":
+      "https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters/p-The-Big-Bang-Theory-Melissa-Rauchi.jpg",
+    "scores": [4, 4, 2, 3, 2, 2, 3, 2, 4, 5]
+  },
+  {
+    "name": "Amy",
+    "photo":
+      "http://wwwimage1.cbsstatic.com/thumbnails/photos/770xh/tbbt-amy.jpg",
+    "scores": [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+  },
+  {
+    "name": "Stuart",
+    "photo":
+      "http://cld.fame10.com/image/upload/t_tn,f_auto,q_auto,$h_400,$w_620/f10/2016/02/Stuart-Bloom-620x400.jpg",
+    "scores": [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+  },
+  {
+    "name": "Barry",
+  "photo":
+      "https://vignette.wikia.nocookie.net/bigbangtheory/images/6/60/Kripkeseason10.png/revision/latest?cb=20161205221818",
+    "scores": [2, 3, 3, 3, 4, 2, 2, 2, 2, 2]
+  },
+  {
+    "name": "Leslie",
+  "photo":
+      "http://www.writeups.org/wp-content/uploads/Leslie-Winkle-Big-Bang-Theory-Sara-Gilbert-f.jpg",
+    "scores": [3, 3, 5, 5, 2, 1, 3, 3, 3, 3]
+  }
+];
+
+module.exports = friendsArray;
